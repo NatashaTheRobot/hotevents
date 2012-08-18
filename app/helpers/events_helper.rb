872b6e534397c247c5016258fca77006bde74dfb
Nick O'Neill <mr.nick.oneill@gmail.com>
2012-08-18 16:58:38 -0700

    Add a date_from_facebook helper

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -2,4 +2,9 @@ module EventsHelper
 	def get_facebook_image object_id , type = 'square' #can be square, small, normal, or large
 		"http://graph.facebook.com/#{object_id}/picture?type=#{type}"
 	end
+
+	def date_from_facebook date_str
+		#ISO-8601 
+		Time.parse date_str
+	end
 end
