e8b53bd0721bc4e8856e8ef0174add98769a8ff5
Nick O'Neill <mr.nick.oneill@gmail.com>
2012-08-18 16:48:32 -0700

    Addded a Facebook image call for natasha

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -1,2 +1,5 @@
 module EventsHelper
+	def get_facebook_image object_id , type = 'square' #can be square, small, normal, or large
+		"http://graph.facebook.com/#{object_id}/picture?type=#{type}"
+	end
 end
