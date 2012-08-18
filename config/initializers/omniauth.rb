b5493a7fa863897e1de141e294f6ade41be7ba2b
NatashaMurashev <nmurashev@gmail.com>
2012-08-18 15:29:05 -0700

    updated app permissions

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,3 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET']
+  provider :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET'], scope: "user_events, friends_events, friends_location, rsvp_event, offline_access"
 end
