3ee94368a9e34fb4135e845e3e09ca3d1dd9349f
NatashaMurashev <nmurashev@gmail.com>
2012-08-18 10:57:36 -0700

    initial commit

---
+++ Gemfile
@@ -0,0 +1,22 @@
+source :rubygems
+
+gem 'rails', '3.2.7'
+gem 'pg', '0.14.0'
+gem 'haml', '3.1.6'
+
+
+group :development, :test do
+  gem 'rspec-rails', '2.10.0'
+  gem 'annotate', '2.5.0'
+  gem 'quiet_assets', '1.0.1'
+end
+
+# Gems used only for assets and not required
+# in production environments by default.
+group :assets do
+  gem 'sass-rails',   '3.2.5'
+  gem 'coffee-rails', '3.2.2'
+  gem 'uglifier', '1.2.7'
+end
+
+gem 'jquery-rails', '2.0.2'
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hotevents::Application.config.secret_token = '3e101958277efaa4c5f39aa4c8c741f5935dc6d9882155aea17bffc4fcd11ea55a05639516d967862ad4d34eb07896b7a89388ff43c9b8e71ef6bc7c44f85bf5'
