05f883889083d1e7a502cd25f14796e28e2b9ec1
NatashaMurashev <nmurashev@gmail.com>
2012-08-18 14:42:32 -0700

    added events controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Hotevents::Application.routes.draw do
-  root to: "sessions#new"
-  resource :sessions
+  root to: "events#index"
+  resource :events
   get '/auth/facebook/callback', to: 'sessions#create'
   get '/signout' => 'sessions#destroy', as: :signout
 end
