e17f6cb1988589719d7cfc0005f620f55ff1a1f4
NatashaMurashev <nmurashev@gmail.com>
2012-08-19 14:32:51 -0700

    updated environment variable

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,3 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET'], scope: "user_events, friends_events, friends_location, rsvp_event, offline_access"
+  provider :facebook, '338513749570080', 'a899cba3a0a91fb956ae0d745a0f1fc8', scope: "user_events, friends_events, friends_location, rsvp_event, offline_access"
 end
