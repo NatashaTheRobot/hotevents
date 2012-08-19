0d7a9708b301e8a91abdfa3a62334655204d051a
NatashaMurashev <nmurashev@gmail.com>
2012-08-19 13:05:33 -0700

    debugging javascript

--- app/assets/javascripts/events.js
+++ app/assets/javascripts/events.js
@@ -1,38 +1,38 @@
-$(function(){
-
-  var $container = $('#eventcontainer');
-
-  $container.imagesLoaded(function(){
-      $container.masonry({
-         itemSelector: '.eventbox',
-       });
-     });
-
-  $container.masonry({
-       itemSelector : '.eventbox',
-       isAnimated: true
-			 columnWidth: function( containerWidth ) {
-		    return containerWidth / 4;
-		  }
-   });
-
-  $container.infinitescroll(
-    {
-      navSelector  : '#page-nav',    // selector for the paged navigation
-      nextSelector : '#page-nav a',  // selector for the NEXT link (to page 2)
-      itemSelector : '.eventbox',     // selector for all items you'll retrieve
-      animate      : true,
-      debug        : true,
-      loadingText  : "loading..."
-    },
-
-    function( newElements ) {
-      var $newElems = $( newElements ).css({ opacity: 0 });
-      $newElems.animate({ opacity: 1 });
-      $container.masonry( 'appended', $newElems, true );
-    }
-  );
-
-
-
-});
+// $(function(){
+// 
+//   var $container = $('#eventcontainer');
+// 
+//   $container.imagesLoaded(function(){
+//       $container.masonry({
+//          itemSelector: '.eventbox',
+//        });
+//      });
+// 
+//   $container.masonry({
+//        itemSelector : '.eventbox',
+//        isAnimated: true
+// 			 columnWidth: function( containerWidth ) {
+// 		    return containerWidth / 4;
+// 		  }
+//    });
+// 
+//   $container.infinitescroll(
+//     {
+//       navSelector  : '#page-nav',    // selector for the paged navigation
+//       nextSelector : '#page-nav a',  // selector for the NEXT link (to page 2)
+//       itemSelector : '.eventbox',     // selector for all items you'll retrieve
+//       animate      : true,
+//       debug        : true,
+//       loadingText  : "loading..."
+//     },
+// 
+//     function( newElements ) {
+//       var $newElems = $( newElements ).css({ opacity: 0 });
+//       $newElems.animate({ opacity: 1 });
+//       $container.masonry( 'appended', $newElems, true );
+//     }
+//   );
+// 
+// 
+// 
+// });
